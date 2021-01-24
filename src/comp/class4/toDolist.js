9565ab97e9b76e5290cb497357755d57c884a4f5
Ansukun5852 <gouda.ansukun5852.com>
2021-01-24 21:08:55 +0530

    Added css fonts and undo feature

--- src/comp/class4/toDolist.js
+++ src/comp/class4/toDolist.js
@@ -1,46 +1,81 @@
-import React from "react";
-import { Button, Grid, TextField } from "@material-ui/core";
-import "./todolist.css"
+import React from 'react';
+import { Button,TextField } from "@material-ui/core";
 export default class Todolist extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      task: ["Clean dishes"],
-      task1: [],
-      item: "",
-    };
-  }
-  handleChange = (e) => {
-    this.setState({
-      item: e.target.value,
-    });
-  };
-  handleDelete = (e, index) => {
-    var temp = this.state.task;
-    var ele = temp.splice(index, 1);
-    console.log(ele[0]);
-    var temp2 = this.state.task1;
-    temp2.push(ele[0]);
-    this.setState({
-      task2: temp2,
-      task: temp,
-    });
-  };
-  handlePush = () => {
-    var temp = this.state.task;
-    temp.push(this.state.item);
-    this.setState({
-      task: temp,
-      item: "",
-    });
-  };
-  render() {
-    return (
-      <div>
-        <Grid classname = "input" container direction="row" justify="center" alignItems="center">
-          <TextField
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            tasksArr: [ "Create a react App"],
+            taskInput: "",
+            tasksCompleted: ["Done with deployement of the reatc  app"]
+
+        }
+    }
+    handleInput = (e) => {
+        this.setState({
+            taskInput: e.target.value
+        })
+    }
+    pushTask = () => {
+        var temp = this.state.tasksArr;
+        temp.push(this.state.taskInput)
+        this.setState({
+            tasksArr: temp,
+            taskInput: ""
+        })
+    }
+    handleComplete = (e, index) => {
+        var temp = this.state.tasksArr;
+        var ele = temp.splice(index, 1);
+        var temp2 = this.state.tasksCompleted;
+        temp2.push(ele[0])
+        console.log(ele[0]);
+        this.setState({
+            tasksArr: temp,
+            tasksCompleted: temp2
+        })
+    }
+
+    handleUndo = (e, index) => {
+        var temp = this.state.tasksCompleted;
+        var ele = temp.splice(index, 1);
+        var temp2 = this.state.tasksArr;
+        temp2.push(ele[0])
+        console.log(ele[0]);
+        this.setState({
+            tasksArr: temp2,
+            tasksCompleted: temp
+        })
+    }
+
+    handleEnter = (e) => {
+        var code = e.keyCode || e.which;
+        if (code === 13) {
+            this.pushTask()
+
+        }
+
+    }
+
+
+    render() {
+        return (
+            <div style={{
+              padding: 50,
+              borderRadius: 10,
+              justifyContent: "center",
+              marginLeft : 400 ,
+              height:400,
+              width : 400,
+              
+              
+           
+          }} >
+      
+                <h1>To Do lists</h1>
+                <TextField
             id="standard-basic"
-            label="Enter the task to be done"
+            label="Task"
             onChange={this.handleChange}
           />
           <Button
@@ -56,52 +91,75 @@ export default class Todolist extends React.Component {
           >
             Add
           </Button>
-        </Grid>
 
-        <Grid
-          container
-          direction="row"
-          justify="space-evenly"
-          alignItems="center"
-        >
-            <Grid>
-            {this.state.task.length === 0 ?
-                    <h2> Completed</h2> :
-                    <h2>Tasks</h2>}
-                <div className = "completed">
-                    <ul >
-                        {this.state.task.map((single, index) => {
+                {this.state.tasksArr.length === 0 ?
+                    <h2>You have No tasks to do</h2> :
+                    <h2>Tasks to do</h2>}
+                <div style={{
+                    backgroundColor: "#f2f2f2",
+                    padding: 20,
+                    borderRadius: 10,
+                    width: 200,
+                    justifyContent: "center",
+                    alignItems: "center",
+                    display: 'flexbox',
+                    color: 'blueviolet',
+                    fontfamily:  "cursive",
+
+
+                }}>
+                    <ul style={{
+                        padding: 20,
+                        borderRadius: 10,
+                        justifyContent: "center",
+                        alignItems: "center",
+                        display: 'flexbox'
+                    }}>
+                        {this.state.tasksArr.map((single, index) => {
                             return (
                                 <li>
-                                    {single}<br/><Button color="primary" variant="contained" size="small" onClick={(e) => {
-                                        this.handleDelete(e, index, single)
+                                    {single}<br /><Button color="primary" variant="contained" onClick={(e) => {
+                                        this.handleComplete(e, index, single)
                                     }}>Completed</Button>
                                 </li>
                             )
                         })}
                     </ul>
                 </div>
-            </Grid>
 
-                <Grid>
-                {this.state.task1.length === 0 ?
-                    <h2> Completed</h2> :
-                    <h2> task</h2>}
-                <div className = "completed">
-                    <ul >
-                        {this.state.task1.map((single, index) => {
+                {this.state.tasksCompleted.length === 0 ?
+                    <h2>Please Complete The Task With Due Time</h2> :
+                    <h2>Tasks Completed</h2>}
+                <div style={{
+                    backgroundColor: "#f2f2f2",
+                    padding: 20,
+                    borderRadius: 10,
+                    width: 200,
+                    justifyContent: "center",
+                    alignItems: "center",
+                    display: 'flexbox'
+                }}>
+                    <ul style={{
+                        padding: 20,
+                        borderRadius: 10,
+                        justifyContent: "center",
+                        alignItems: "center",
+                        display: 'flexbox'
+                    }}>
+                        {this.state.tasksCompleted.map((single, index) => {
                             return (
                                 <li>
-                                  <strike>{single}<br/></strike>  
+                                    {single}<br /><Button color="primary" variant="contained" onClick={(e) => {
+                                        this.handleUndo(e, index)
+                                    }}>Undo</Button>
                                 </li>
                             )
                         })}
                     </ul>
                 </div>
-                </Grid>
 
-        </Grid>
-      </div>
-    );
-  }
+
+            </div>
+        )
+    }
 }
